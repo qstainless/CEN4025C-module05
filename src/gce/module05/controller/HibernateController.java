a32fa7a1840c522c6e948cc1a77bbc6427e9eec2
Queue Stainless <gce517+git@gmail.com>
2020-05-26 15:06:00 -0400

    Added HibernateController and hibernate configuration file.

---
+++ src/gce/module05/controller/HibernateController.java
@@ -0,0 +1,52 @@
+package gce.module05.controller;
+
+import gce.module05.model.Item;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateController {
+
+    private static SessionFactory sessionFactory = null;
+
+    static {
+        try {
+            loadSessionFactory();
+        } catch (Exception e) {
+            System.err.println("Exception while initializing hibernate controller.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void loadSessionFactory() {
+        Configuration configuration = new Configuration();
+
+        // Defaults to src/hibernate.cfg.xml
+        configuration.configure().addAnnotatedClass(Item.class);
+
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    public static Session getSession() throws HibernateException {
+        Session session = null;
+
+        try {
+            session = sessionFactory.openSession();
+        } catch (Throwable t) {
+            System.err.println("Error opening the session. ");
+
+            t.printStackTrace();
+        }
+
+        if (session == null) {
+            System.err.println("No session exists.");
+        }
+
+        return session;
+    }
+}
